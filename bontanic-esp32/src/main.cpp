240e52cf1483dd7347636bf422d66cc6258d78a3
Frowin <10233904+frowin@users.noreply.github.com>
2025-02-07 14:21:09 +0100

    added libs

--- bontanic-esp32/src/main.cpp
+++ bontanic-esp32/src/main.cpp
@@ -1,7 +1,13 @@
+#include <Arduino.h>
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
 #include <ArduinoOTA.h>
 #include <LittleFS.h>
-    
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "LittleFS.h"
+#include <Arduino_JSON.h>
+
 // Forward declaration of readHelloWorld
 void readHelloWorld();
 
@@ -9,30 +15,12 @@ WiFiManager wm;
 
 
 void setup() {
-    // WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
-    // it is a good practice to make sure your code sets wifi mode how you want it.
 
-    // put your setup code here, to run once:
     Serial.begin(115200);
     
-    //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
-
+    WiFi.setHostname("bontanic"); // For ESP32
     
-    // Set device hostname before connection
-    WiFi.hostname("bontanic"); // For ESP8266
-    // WiFi.setHostname("bontanic"); // For ESP32
-    
-    // reset settings - wipe stored credentials for testing
-    // these are stored by the esp library
-    // wm.resetSettings();
-
-    // Automatically connect using saved credentials,
-    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
-    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
-    // then goes into a blocking loop awaiting configuration and will return success result
-
     bool res;
-    // res = wm.autoConnect(); // auto generated AP name from chipid
     res = wm.autoConnect("AutoConnectAP"); // anonymous ap
     if(!res) {
         Serial.println("Failed to connect");
