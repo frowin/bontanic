187023000fcc94cc1e23ddbe59a6391507a33751
Frowin <10233904+frowin@users.noreply.github.com>
2025-02-07 14:17:54 +0100

    implemented littlefs

--- bontanic-esp32/src/main.cpp
+++ bontanic-esp32/src/main.cpp
@@ -1,4 +1,11 @@
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include <ArduinoOTA.h>
+#include <LittleFS.h>
+    
+// Forward declaration of readHelloWorld
+void readHelloWorld();
+
+WiFiManager wm;
 
 
 void setup() {
@@ -9,8 +16,12 @@ void setup() {
     Serial.begin(115200);
     
     //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
-    WiFiManager wm;
 
+    
+    // Set device hostname before connection
+    WiFi.hostname("bontanic"); // For ESP8266
+    // WiFi.setHostname("bontanic"); // For ESP32
+    
     // reset settings - wipe stored credentials for testing
     // these are stored by the esp library
     // wm.resetSettings();
@@ -22,20 +33,81 @@ void setup() {
 
     bool res;
     // res = wm.autoConnect(); // auto generated AP name from chipid
-    // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
-    res = wm.autoConnect(getenv("WIFI_SSID"),getenv("WIFI_PASSWORD")); // password protected ap
-
+    res = wm.autoConnect("AutoConnectAP"); // anonymous ap
     if(!res) {
         Serial.println("Failed to connect");
         // ESP.restart();
     } 
     else {
         //if you get here you have connected to the WiFi    
-        Serial.println("connected...yeey :)");
+        Serial.println("Connected to WiFi");
+        
+        // OTA Setup
+        ArduinoOTA.setHostname("bontanic"); // Same hostname as above
+        
+        // Optional password for OTA updates
+        // ArduinoOTA.setPassword("admin");
+        
+        ArduinoOTA.onStart([]() {
+            String type;
+            if (ArduinoOTA.getCommand() == U_FLASH) {
+                type = "sketch";
+            } else { // U_FS
+                type = "filesystem";
+            }
+            Serial.println("Start updating " + type);
+        });
+        
+        ArduinoOTA.onEnd([]() {
+            Serial.println("\nEnd");
+        });
+        
+        ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+            Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+        });
+        
+        ArduinoOTA.onError([](ota_error_t error) {
+            Serial.printf("Error[%u]: ", error);
+            if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+            else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+            else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+            else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+            else if (error == OTA_END_ERROR) Serial.println("End Failed");
+        });
+        
+        ArduinoOTA.begin();
+        Serial.println("OTA Ready");
+        Serial.print("IP address: ");
+        Serial.println(WiFi.localIP());
     }
 
+    readHelloWorld();
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:   
+    ArduinoOTA.handle(); // Handle OTA updates
+    //wm.startConfigPortal(); // Blocks until configuration is done or timeout
+
+}
+
+
+void readHelloWorld() {
+    if(!LittleFS.begin(true)){
+    Serial.println("An Error has occurred while mounting LittleFS");
+    return;
+  }
+  
+  File file = LittleFS.open("/text.txt");
+  if(!file){
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+  
+  Serial.println("File Content:");
+  while(file.available()){
+    Serial.write(file.read());
+  }
+  Serial.println("");
+  Serial.println("File closed");
+  file.close();
 }
